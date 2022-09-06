4fde44470c4d4a63066065891269181fc290dfb2
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-09-06 14:00:23 +0300

    Add IP addresses

--- hwtool.py
+++ hwtool.py
@@ -1,5 +1,6 @@
 import psutil
 import sys
+import socket
 from prettytable import PrettyTable
 from psutil._common import bytes2human
 
@@ -11,6 +12,7 @@ def getTemps():
 
     for name, entries in temps.items():
         x = PrettyTable()
+        x.title = "TEMPERATURE SENSORS"
         x.field_names = ["Sensor", "Temperature", "High", "Critical"]
         for entry in entries:
             x.add_row(
@@ -19,8 +21,8 @@ def getTemps():
         print(x)
 
 def getDiskSpace():
-
     x = PrettyTable()
+    x.title = "DISK USAGE"
     x.field_names = ["Device", "Total", "Used", "Free", "Use", "Type", "Mount"]
     parts = psutil.disk_partitions(all=False)
     for part in parts:
@@ -30,9 +32,21 @@ def getDiskSpace():
         )
     print (x)
 
+def getNetworkInfo():
+    addrs = psutil.net_if_addrs()
+    x = PrettyTable()
+    x.title = "NETWORK INFORMATION"
+    x.field_names = ["Device", "IPv4 Address", "Netmask"]
+    for nic, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET:
+                x.add_row( [nic, addr.address, addr.netmask or "N/A"] )
+    print(x)    
+
 def main():
     getTemps()
     getDiskSpace()
+    getNetworkInfo()
 
 if __name__ == "__main__":
     sys.exit(main())
